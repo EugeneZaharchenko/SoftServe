9fa854e0ee359cb4290586e8290e01694c0eaa46
J0nnyZ <EugeneZaharchenko@users.noreply.github.com>
2019-05-15 16:18:16 +0300

    task7, task8 with tests

--- elementary tasks/task8fold/task8.py
+++ elementary tasks/task8fold/task8.py
@@ -1,19 +1,20 @@
 """
-Elementary Task #8
+Elementary Task 8
 
-Ряд Фибоначчи для диапазона
+Fibonacci sequence for a certain range
 ----------------
 
-Программа позволяет вывести все числа Фибоначчи, которые находятся в указанном диапазоне.
-Диапазон задаётся двумя аргументами при вызове главного класса.
-Числа выводятся через запятую по возрастанию.
+The program lets to output all Fibonacci numbers, which are in given range.
+The range is given with 2 arguments during the main class call.
+Fibonacci numbers are comma separated in output and sorted by asc.
+
 """
 
 import sys
 
 
 class Fibonacci:
-    def __init__(self, start=0, stop=1):
+    def __init__(self, start, stop):
         if stop > start:
             self.start = int(start)
             self.stop = int(stop)
@@ -23,15 +24,15 @@ class Fibonacci:
 
     def fib(self, n):
         phi = ((1 + (5 ** (1 / 2))) / 2)
-        return str(round((phi ** n) / (5 ** (1 / 2))))
+        return round((phi ** n) / (5 ** (1 / 2)))
 
     def sequence(self):
         for n in range(self.start, self.stop + 1):
-            self.fib_seq.append(self.fib(n))
+            self.fib_seq.append(str(self.fib(n)))
 
     def __str__(self):
         self.sequence()
-        return "Fibonacci sequence for a given range {start}-{stop} is: {seq}".format(start=self.stop, stop=self.stop,
+        return "Fibonacci sequence for a given range {start}-{stop} is: {seq}".format(start=self.start, stop=self.stop,
                                                                                       seq=', '.join(self.fib_seq))
 
 
@@ -44,7 +45,6 @@ def main():
         # creating instance of Fibonacci class in certain range which is given as arguments
         fib = Fibonacci(sys.argv[1], sys.argv[2])
         print(fib)
-        exit()
     except ValueError:
         print("Wrong arguments. Give start and stop values where stop is bigger (start < stop)")
     except IndexError:
---
+++ elementary tasks/task8fold/test8.py
@@ -0,0 +1,46 @@
+import unittest
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from task8fold.task8 import Fibonacci
+
+
+class TestFibonacciClass(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.num1 = Fibonacci(0, 1)
+        self.num1_5 = Fibonacci(1, 5)
+        self.num5_9 = Fibonacci(5, 9)
+
+    def tearDown(self):
+        print('Task 8 tested')
+
+    @unittest.expectedFailure
+    def test_validate(self):
+        self.assertIs(self.num1.start, self.num1.stop, int)
+        self.assertIs(self.num1_5.start, self.num1_5.stop, int)
+        self.assertIs(self.num5_9.start, self.num5_9.stop, int)
+
+    def test_instance(self):
+        self.assertIsInstance(self.num1, Fibonacci)
+        self.assertIsInstance(self.num1_5, Fibonacci)
+        self.assertIsInstance(self.num5_9, Fibonacci)
+
+    def test_exact_val(self):
+        self.assertEqual(self.num1.fib(0), 0)
+        self.assertEqual(self.num1.fib(1), 1)
+        self.assertEqual(self.num1_5.fib(5), 5)
+        self.assertEqual(self.num5_9.fib(9), 34)
+
+    def test_seq(self):
+        self.assertEqual(self.num1.sequence(), None)
+
+    def test_str(self):
+        self.assertEqual(self.num1.__str__(), 'Fibonacci sequence for a given range 0-1 is: 0, 1')
+        self.assertEqual(self.num1_5.__str__(), 'Fibonacci sequence for a given range 1-5 is: 1, 1, 2, 3, 5')
+        self.assertEqual(self.num5_9.__str__(), 'Fibonacci sequence for a given range 5-9 is: 5, 8, 13, 21, 34')
+
+
+if __name__ == "__main__":
+    unittest.main()
